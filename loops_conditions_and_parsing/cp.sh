281f5ef9d6dba58eb5982981fc1a7439c26e4d8a
bbenoit-droid <b.benoit@alustudent.com>
2026-01-31 11:08:16 -0800

    new update 2026-01-31_11-08-16

--- loops_conditions_and_parsing/cp.sh
+++ loops_conditions_and_parsing/cp.sh
@@ -3,5 +3,5 @@ count=0
 for f in /alu-shell/alu-shell/loops_conditions_and_parsing/*; do
   cp "$f" /alu-shell/loops_conditions_and_parsing/
   ((count++))
-  [[ $count -ge 4 ]] && break
+  [[ $count -ge 12 ]] && break
 done
